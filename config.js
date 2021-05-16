0f051c0b6ca74ba6a57aeffbf3bbf7047f59298a
WoobaReplit <>
2021-05-16 13:45:35 +0000

    Token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	prefix: '-',
 	owners: ['623299563150639126'],
-	token: 'ODQzMTc5MDg3NjIyMzA3OTAw.YKAFtw.VRUyAcqNEMtIoY-SSOItt51RNRM'
+	token: 'ODQzMTc5MDg3NjIyMzA3OTAw.YKAFtw.Cx6HuUWnSv5NTsRagt7cIB6OIEg'
 };
