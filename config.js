b6e673286f237af8ad9f7654e7d345bde8b46c05
WoobaReplit <>
2021-05-16 13:44:43 +0000

    Added reddit command

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	prefix: '-',
 	owners: ['623299563150639126'],
-	token: 'ODQzMTc5MDg3NjIyMzA3OTAw.YKAFtw.x8cG3FKecyKRSuy23-rw_RT37nQ'
+	token: 'ODQzMTc5MDg3NjIyMzA3OTAw.YKAFtw.VRUyAcqNEMtIoY-SSOItt51RNRM'
 };
